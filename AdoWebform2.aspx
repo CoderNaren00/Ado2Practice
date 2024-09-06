7aef177104bb1f99ce646101117c8c9c53e0cc48
_Coder_Naren <narendrajalamkar8399@gmail.com>
2024-09-06 11:03:54 +0530

    Add project files.

---
+++ AdoWebform2.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AdoWebform2.aspx.cs" Inherits="Ado2Practice.AdoWebform2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Connecting to SQL Server<br />
+            <br />
+            <asp:Button ID="btnConnect" runat="server" OnClick="btnConnect_Click" Text="Connect" />
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="lblMessage" runat="server" Text="Loading..."></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AdoWebform2.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Ado2Practice
+{
+    public partial class AdoWebform2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = @"data source=ASUSNARENDRA;initial catalog=Sample1;intergrated security=true";
+                con.Open();
+
+                lblMessage.Text = "Connected Successfully to SQL Server";
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+            }
+        }
+    }
+}
